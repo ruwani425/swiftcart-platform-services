82f2c0a3a75f589c7da9829e97624263b3212bf6
Ruwani Ranthika <ruwanielv@gmail.com>
2026-08-27 10:30:35 +0530

    feat: add ecosystem.config.js for single command PM2 startup

---
+++ ecosystem.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  apps: [
+    {
+      name: "config-server",
+      script: "java",
+      args: "-Dspring.classformat.ignore=true -jar config-server/target/config-server-1.0.0-SNAPSHOT.jar",
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "768M",
+      env: {
+        NODE_ENV: "production"
+      }
+    },
+    {
+      name: "discovery-server",
+      script: "java",
+      args: "-Dspring.classformat.ignore=true -jar discovery-server/target/discovery-server-1.0.0-SNAPSHOT.jar",
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "768M",
+      env: {
+        NODE_ENV: "production"
+      }
+    },
+    {
+      name: "api-gateway",
+      script: "java",
+      args: "-Dspring.classformat.ignore=true -jar api-gateway/target/api-gateway-1.0.0-SNAPSHOT.jar",
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "768M",
+      env: {
+        NODE_ENV: "production"
+      }
+    }
+  ]
+};
